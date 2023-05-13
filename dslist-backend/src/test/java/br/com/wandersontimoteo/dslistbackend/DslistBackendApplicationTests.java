ce2b337c85574c189b01a0341c3d42e683962996
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-05-13 17:35:36 -0400

    Criado projeto inicial api dslist

---
+++ dslist-backend/src/test/java/br/com/wandersontimoteo/dslistbackend/DslistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wandersontimoteo.dslistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

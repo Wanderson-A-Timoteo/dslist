0564c3468ce05df5284beabf3607a3303e636155
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-05-14 16:41:18 -0400

    Deploy ready

--- dslist-backend/src/main/java/br/com/wandersontimoteo/dslistbackend/repositories/GameListRepository.java
+++ dslist-backend/src/main/java/br/com/wandersontimoteo/dslistbackend/repositories/GameListRepository.java
@@ -2,8 +2,10 @@ package br.com.wandersontimoteo.dslistbackend.repositories;
 
 import br.com.wandersontimoteo.dslistbackend.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 
 }
-
